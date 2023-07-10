f186206fab3dfddc8b15f391c91aa6038711fc76
Minh Hoàng <108507395+hoang051101@users.noreply.github.com>
2023-07-10 23:23:42 +0700

    Update playermove.cs

--- Assets/Sprite/playermove.cs
+++ Assets/Sprite/playermove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playermove : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 5f;
     void Start()
     {
         
@@ -13,6 +14,11 @@ public class playermove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
+        transform.Translate(movement);
+
     }
 }
